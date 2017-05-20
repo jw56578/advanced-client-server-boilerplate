2db38867c7d4d5b42032eeb36803c16e6a891585
Jon Woo <jw56578@gmail.com>
2017-05-20 13:53:52 -0500

    addfiles

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {users: []};
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div>
+        Client
+      </div>
+    );
+  }
+}
+export default (App);
+
+
